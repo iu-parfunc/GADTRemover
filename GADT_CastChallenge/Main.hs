5ec52e0929ed8053ff07716f63e582ff13d49a04
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2015-04-10 17:08:02 +1000

    split GADT definition to separate file

--- GADT_CastChallenge/Main.hs
+++ GADT_CastChallenge/Main.hs
@@ -15,63 +15,8 @@ import Control.Monad (forM_)
 import Data.Dynamic
 import Debug.Trace
 
---------------------------------------------------------------------------------
--- (*) Types
+import GADT
 
--- | Simplest version.  Use a closed world of value types in the Exp:
-data Ty = BoolTy | IntTy | AnyTy
-  deriving (Show, Typeable)
--- The way Accelerate, for example, does this is more complicated,
--- using a class for value types which can reify them.
-
-deriving instance (Typeable 'BoolTy)
-deriving instance (Typeable 'IntTy)
-deriving instance (Typeable 'AnyTy)
-
-class Typeable t => ReifyTy t where
-  reifyTy :: Proxy t -> Ty
-instance ReifyTy 'BoolTy where reifyTy _ = BoolTy
-instance ReifyTy 'IntTy  where reifyTy _ = IntTy
-instance ReifyTy 'AnyTy  where reifyTy _ = AnyTy
-
---------------------------------------------------------------------------------
--- (*) Environments
-
-data Env = EmptyEnv | Extend Ty Env
-
-deriving instance (Typeable 'EmptyEnv)
-deriving instance Typeable 'Extend
-
-type family   ENV_HEAD (t::Env) :: Ty
-type instance ENV_HEAD ('Extend s e) = s
-
---------------------------------------------------------------------------------
--- (1) GADT based AST 
-
-data Exp (env :: Env) (a :: Ty) where
-  T   :: Exp env BoolTy
-  F   :: Exp env BoolTy
-  If  :: Exp env BoolTy -> Exp env a -> Exp env a -> Exp env a
-  Lit :: Int -> Exp env IntTy
-  Add :: Exp env IntTy -> Exp env IntTy -> Exp env IntTy
-  Let :: ReifyTy t1 =>
-         Exp env t1
-      -> Exp (Extend t1 env) a
-      -> Exp env a  
-  Var :: Idx env a -> Exp env a
- deriving Typeable
-
-data Idx (env :: Env) (t :: Ty) where
-  -- Here, again, ReifyTy is redundant, but no way to tell GHC that:
-  Zero :: forall (env :: Env) (t::Ty) . ReifyTy t =>
-          Idx (Extend t env) t
-  Succ :: forall (env :: Env) (s :: Ty) (t :: Ty) . ReifyTy s => 
-          Idx env t -> Idx (Extend s env) t
-  deriving Typeable
-
-deriving instance (Show (Exp env a))
-
-deriving instance (Show (Idx env t))
 
 --------------------------------------------------------------------------------
 -- (2) ADT version:
