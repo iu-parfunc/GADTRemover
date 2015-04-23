fa5cb15f9d8c91add16be7cb9df2f1596999eec6
Ryan Newton <rrnewton@gmail.com>
2015-04-22 22:57:01 -0400

    Remove first approach that lacked value level environments

--- GADT_CastChallenge/Main.hs
+++ GADT_CastChallenge/Main.hs
@@ -1,15 +1,10 @@
 
 module Main where
 
-import qualified Test1
 import qualified Test2
-
 
 main :: IO ()
 main = do
-  putStrLn "===== Approach 1 ====="
-  Test1.runTests
 
   putStrLn "===== Approach 2 ====="
   Test2.runTests
-
