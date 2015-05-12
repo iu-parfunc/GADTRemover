effbeeb4b987b322646ea79c6b535e6cc6f42dd3
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2015-05-12 18:56:20 +1000

    add more Elt types and product reification
    * Test non-injective encodings
    * Reification of IsProduct class

--- GADT_CastChallenge/GADT3.hs
+++ GADT_CastChallenge/GADT3.hs
@@ -1,42 +1,56 @@
+{-# LANGUAGE DeriveDataTypeable   #-}
+{-# LANGUAGE FlexibleContexts     #-}
+{-# LANGUAGE FlexibleInstances    #-}
 {-# LANGUAGE GADTs                #-}
 {-# LANGUAGE ScopedTypeVariables  #-}
 {-# LANGUAGE TypeFamilies         #-}
 {-# LANGUAGE TypeOperators        #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
 
 module GADT3
   where
 
+import Data.Typeable
 import Text.PrettyPrint.Leijen
 
 
 -- Product types
 -- =============
 
+data ProdR p where
+  ProdRzero ::                     ProdR ()
+  ProdRsnoc :: Elt e => ProdR p -> ProdR (p, e)
+
 class IsProduct t where
   type ProdRepr t
   fromProd :: t -> ProdRepr t
   toProd   :: ProdRepr t -> t
+  prodR    :: {- dummy -} t -> ProdR (ProdRepr t)
 
 instance IsProduct () where
   type ProdRepr () = ()
   fromProd () = ()
   toProd ()   = ()
+  prodR _     = ProdRzero
 
-instance IsProduct (a, b) where
+instance (Elt a, Elt b) => IsProduct (a, b) where
   type ProdRepr (a, b) = (((), a), b)
   fromProd (a, b)     = (((), a), b)
   toProd (((), a), b) = (a, b)
+  prodR _             = ProdRsnoc (ProdRsnoc ProdRzero)
 
-instance IsProduct (a, b, c) where
+instance (Elt a, Elt b, Elt c) => IsProduct (a, b, c) where
   type ProdRepr (a, b, c) = (ProdRepr (a, b), c)
   fromProd (a, b, c) = (fromProd (a,b), c)
   toProd (ab, c)     = let (a, b) = toProd ab in (a, b, c)
+  prodR _            = ProdRsnoc (prodR (undefined :: (a,b)))
 
-instance IsProduct (a, b, c, d) where
+instance (Elt a, Elt b, Elt c, Elt d) => IsProduct (a, b, c, d) where
   type ProdRepr (a, b, c, d) = (ProdRepr (a, b, c), d)
   fromProd (a, b, c, d) = (fromProd (a,b,c), d)
   toProd (abc, d)       = let (a, b, c) = toProd abc in (a, b, c, d)
+  prodR _               = ProdRsnoc (prodR (undefined :: (a,b,c)))
 
 
 -- Type hierarchy
@@ -97,27 +111,27 @@ instance Show (NonNumType a) where
 -- Surface and representation types
 -- ================================
 
-type family EltRepr a :: *
-type instance EltRepr () = ()
-type instance EltRepr Int = Int
-type instance EltRepr Bool = Bool
-type instance EltRepr Float = Float
-type instance EltRepr (a, b) = ProdRepr (EltRepr a, EltRepr b)
-type instance EltRepr (a, b, c) = ProdRepr (EltRepr a, EltRepr b, EltRepr c)
-type instance EltRepr (a, b, c, d) = ProdRepr (EltRepr a, EltRepr b, EltRepr c, EltRepr d)
-
--- type instance EltRepr Z              = ()
--- type instance EltRepr (tail :. head) = ProdRepr (EltRepr tail, EltRepr head)
+-- The representation types are _closed_. We convert everything into this
+-- format, and the untyped version will work in this format (hopefully).
 --
--- data Z            = Z
--- data tail :. head = tail :. head
+type family EltRepr a where
+  EltRepr ()           = ()
+  EltRepr Int          = Int
+  EltRepr Bool         = Bool
+  EltRepr Float        = Float
+  EltRepr (a, b)       = ProdRepr (EltRepr a, EltRepr b)
+  EltRepr (a, b, c)    = ProdRepr (EltRepr a, EltRepr b, EltRepr c)
+  EltRepr (a, b, c, d) = ProdRepr (EltRepr a, EltRepr b, EltRepr c, EltRepr d)
 
-data TypeR a where
-  TypeRzero      ::                       TypeR ()
-  TypeRscalar    :: ScalarType a ->       TypeR a
-  TypeRsnoc      :: TypeR a -> TypeR b -> TypeR (a, b)
+  -- Bonus: shapes introduce non-injectivity into the representation types
+  EltRepr Z               = ()
+  EltRepr (t:.h)          = (EltRepr t, EltRepr h)
+  EltRepr All             = ()
+  EltRepr (Any Z)         = ()
+  EltRepr (Any (sh:.Int)) = (EltRepr (Any sh), ())
 
-class Show a => Elt a where
+
+class (Show a, Typeable a, Typeable (EltRepr a)) => Elt a where
   toElt   :: EltRepr a -> a
   fromElt :: a -> EltRepr a
   eltType :: {- dummy -} a -> TypeR (EltRepr a)
@@ -143,25 +157,86 @@ instance Elt Bool where
   eltType _ = TypeRscalar (NonNumScalarType (TypeBool NonNumDict))
 
 instance (Elt a, Elt b) => Elt (a, b) where
-  toElt p        = let (a, b) = toProd p in (toElt a, toElt b)
-  fromElt (a, b) = fromProd (fromElt a, fromElt b)
-  eltType _      = TypeRzero `TypeRsnoc` eltType (undefined :: a)
-                             `TypeRsnoc` eltType (undefined :: b)
+  toElt (((),a),b) = (toElt a, toElt b)
+  fromElt (a, b)   = (((),fromElt a), fromElt b)
+  eltType _        = TypeRzero `TypeRsnoc` eltType (undefined :: a)
+                               `TypeRsnoc` eltType (undefined :: b)
 
 instance (Elt a, Elt b, Elt c) => Elt (a, b, c) where
-  toElt p           = let (a, b, c) = toProd p in (toElt a, toElt b, toElt c)
-  fromElt (a, b, c) = fromProd (fromElt a, fromElt b, fromElt c)
-  eltType _         = TypeRzero `TypeRsnoc` eltType (undefined :: a)
-                                `TypeRsnoc` eltType (undefined :: b)
-                                `TypeRsnoc` eltType (undefined :: c)
-
-instance (Elt a, Elt b, Elt c, Elt d) => Elt (a, b, c, d) where
-  toElt p              = let (a, b, c, d) = toProd p in (toElt a, toElt b, toElt c, toElt d)
-  fromElt (a, b, c, d) = fromProd (fromElt a, fromElt b, fromElt c, fromElt d)
+  toElt ((((),a),b),c) = (toElt a, toElt b, toElt c)
+  fromElt (a, b, c)    = ((((),fromElt a), fromElt b), fromElt c)
   eltType _            = TypeRzero `TypeRsnoc` eltType (undefined :: a)
                                    `TypeRsnoc` eltType (undefined :: b)
                                    `TypeRsnoc` eltType (undefined :: c)
-                                   `TypeRsnoc` eltType (undefined :: d)
+
+instance (Elt a, Elt b, Elt c, Elt d) => Elt (a, b, c, d) where
+  toElt (((((),a),b),c),d) = (toElt a, toElt b, toElt c, toElt d)
+  fromElt (a, b, c, d)     = (((((),fromElt a), fromElt b), fromElt c), fromElt d)
+  eltType _                = TypeRzero `TypeRsnoc` eltType (undefined :: a)
+                                       `TypeRsnoc` eltType (undefined :: b)
+                                       `TypeRsnoc` eltType (undefined :: c)
+                                       `TypeRsnoc` eltType (undefined :: d)
+
+-- Bonus: shapes
+-- -------------
+
+type DIM0 = Z
+type DIM1 = DIM0 :. Int
+type DIM2 = DIM1 :. Int
+type DIM3 = DIM2 :. Int
+
+data Z = Z
+  deriving (Show, Typeable)
+
+infixl 3 :.
+data tail :. head = tail :. head
+  deriving (Show, Typeable)
+
+data All = All
+  deriving (Show, Typeable)
+
+data Any sh = Any
+  deriving (Show, Typeable)
+
+instance Elt Z where
+  toElt ()  = Z
+  fromElt Z = ()
+  eltType _ = TypeRzero         -- doesn't need a value-level representation (??)
+
+instance Elt All where
+  toElt ()    = All
+  fromElt All = ()
+  eltType _   = TypeRscalar (NonNumScalarType TypeUnit)
+
+instance Elt (Any Z) where
+  toElt _   = Any
+  fromElt _ = ()
+  eltType _ = TypeRzero         -- doesn't need a value-level representation (??)
+
+instance Shape sh => Elt (Any (sh:.Int)) where
+  toElt _   = Any
+  fromElt _ = (fromElt (undefined :: Any sh), ())
+  eltType _ = eltType (undefined :: (Any sh)) `TypeRsnoc` TypeRscalar (NonNumScalarType TypeUnit)
+
+instance (Elt t, Elt h) => Elt (t:.h) where
+  toElt (t,h)    = toElt t :. toElt h
+  fromElt (t:.h) = (fromElt t, fromElt h)
+  eltType _      = eltType (undefined :: t) `TypeRsnoc` eltType (undefined :: h)
+
+
+class (Elt sh, Elt (Any sh)) => Shape sh
+
+instance Shape Z
+instance Shape sh => Shape (sh:.Int)
+
+
+-- Type refication
+-- ---------------
+
+data TypeR a where
+  TypeRzero      ::                       TypeR ()
+  TypeRscalar    :: ScalarType a ->       TypeR a
+  TypeRsnoc      :: TypeR a -> TypeR b -> TypeR (a, b)
 
 instance Show (TypeR a) where
   show = show . ppTypeR'
@@ -192,6 +267,12 @@ t4 = eltType (undefined :: (Int,Int,Int,Float))
 t5 = eltType (undefined :: (Bool, (Int,Float), Int))
 t6 = eltType (undefined :: ((Int,Int),(Int,Float)))
 t7 = eltType (undefined :: (((Int,Int),(Int,Float)),Float))
+
+t8 = eltType (undefined :: DIM1)
+t9 = eltType (undefined :: DIM2)
+t10 = eltType (undefined :: Any (DIM2))
+t11 = eltType (undefined :: Z :. Int :. All)
+t12 = eltType (undefined :: Z :. All :. Int)
 
 
 -- Language definition
@@ -424,4 +505,19 @@ p10 = constant (1, (4,2), True)
 p11 :: Exp (Float,Int)
 p11 = Let p10
     $ Prj (SuccProdIdx ZeroProdIdx) (Var ZeroIdx)
+
+p12 :: Exp DIM1
+p12 = constant (Z :. 10)
+
+p13 :: Exp (Any DIM2)
+p13 = constant Any
+
+p14 :: Exp ((Bool,Int), Float)
+p14 = constant ((True,42), pi)
+
+p15 :: Exp (Bool,Int,Float)
+p15 = constant (True,1,2)
+
+p16 :: Exp (Bool, (Int,Float))
+p16 = constant (False, (12,42))
 
