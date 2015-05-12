be345a4fa8da29ba39f077abdfa12a8987194e57
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2015-05-12 11:47:42 +1000

    add upcast GADT3 -> ADT3

--- GADT_CastChallenge/ADT3.hs
+++ GADT_CastChallenge/ADT3.hs
@@ -4,6 +4,9 @@
 module ADT3
   where
 
+import Prelude                                  hiding ( exp )
+import qualified GADT3                          as GADT
+
 
 -- Untyped definition
 -- ==================
@@ -86,7 +89,7 @@ evalOpenExp env = go
                             PrimToFloat -> toFloat (go x)
 
     prj :: TupleIdx -> Val -> Val
-    prj ix (VTup vs) = vs !! ix
+    prj ix (VTup vs) = reverse vs !! ix
     prj _ _          = error "Prj: expected tuple"
 
     if_ :: Val -> Val -> Val -> Val
@@ -109,6 +112,7 @@ evalOpenExp env = go
     toFloat _        = error "PrimToFloat: incorrect arguments"
 
 
+{--
 -- Tests
 -- -----
 
@@ -184,33 +188,107 @@ p10 = constant ((1, (4,2), True) :: (Int, (Float, Int), Bool))
 p11 :: Exp
 p11 = Let p10
     $ Prj 1 (Var TUnit 0)       -- type error!
+--}
 
 
 -- Upcasting
 -- =========
 
-{--
-upcastTupleType :: GADT.TupleType t -> Type
-upcastTupleType GADT.UnitTuple         = TUnit
-upcastTupleType (GADT.PairTuple ta tb) = TPair (upcastTupleType ta) (upcastTupleType tb)
-upcastTupleType (GADT.SingleTuple t)   = upcastScalarType t
+-- Types
 
 upcastScalarType :: GADT.ScalarType a -> Type
-upcastScalarType (GADT.NumScalarType t)    = upcastNumScalarType t
+upcastScalarType (GADT.NumScalarType t)    = upcastNumType t
 upcastScalarType (GADT.NonNumScalarType t) = upcastNonNumScalarType t
 
-upcastNumScalarType :: GADT.NumType a -> Type
-upcastNumScalarType (GADT.IntegralNumType t) = upcastIntegralNumType t
-upcastNumScalarType (GADT.FloatingNumType t) = upcastFloatingNumType t
+upcastNumType :: GADT.NumType t -> Type
+upcastNumType (GADT.IntegralNumType t) = upcastIntegralNumType t
+upcastNumType (GADT.FloatingNumType t) = upcastFloatingNumType t
 
-upcastIntegralNumType :: GADT.IntegralType a -> Type
+upcastIntegralNumType :: GADT.IntegralType t -> Type
 upcastIntegralNumType GADT.TypeInt{} = TInt
 
-upcastFloatingNumType :: GADT.FloatingType a -> Type
+upcastFloatingNumType :: GADT.FloatingType t -> Type
 upcastFloatingNumType GADT.TypeFloat{} = TFloat
 
-upcastNonNumScalarType :: GADT.NonNumType a -> Type
+upcastNonNumScalarType :: GADT.NonNumType t -> Type
+upcastNonNumScalarType GADT.TypeUnit   = TUnit
 upcastNonNumScalarType GADT.TypeBool{} = TBool
---}
 
+upcastTypeR :: GADT.TypeR t -> Type
+upcastTypeR = tup . go
+  where
+    tup []  = error "empty type!"
+    tup [x] = x
+    tup xs  = TTup xs
+
+    go :: GADT.TypeR a -> [Type]
+    go GADT.TypeRzero                                         = []
+    go (GADT.TypeRscalar t)                                   = [ upcastScalarType t ]
+    go (GADT.TypeRsnoc GADT.TypeRzero b@GADT.TypeRsnoc{})     = [ tup (go b) ]
+    go (GADT.TypeRsnoc a@GADT.TypeRsnoc{} b@GADT.TypeRsnoc{}) = [ tup (go a), tup (go b) ]
+    go (GADT.TypeRsnoc a b)                                   = go a ++ go b
+
+
+-- Values
+
+upcastScalar :: GADT.ScalarType t -> t -> Val
+upcastScalar (GADT.NumScalarType t)    x = upcastNumScalar t x
+upcastScalar (GADT.NonNumScalarType t) x = upcastNonNumScalar t x
+
+upcastNumScalar :: GADT.NumType t -> t -> Val
+upcastNumScalar (GADT.IntegralNumType t) x = upcastIntegralNum t x
+upcastNumScalar (GADT.FloatingNumType t) x = upcastFloatingNum t x
+
+upcastIntegralNum :: GADT.IntegralType t -> t -> Val
+upcastIntegralNum GADT.TypeInt{} = VInt
+
+upcastFloatingNum :: GADT.FloatingType t -> t -> Val
+upcastFloatingNum GADT.TypeFloat{} = VFloat
+
+upcastNonNumScalar :: GADT.NonNumType t -> t -> Val
+upcastNonNumScalar GADT.TypeUnit   = const VUnit
+upcastNonNumScalar GADT.TypeBool{} = VBool
+
+upcastConstR :: GADT.TypeR t -> t -> Val
+upcastConstR ty = tup . go ty
+  where
+    tup []  = error "empty value!"
+    tup [x] = x
+    tup xs  = VTup xs
+
+    go :: GADT.TypeR a -> a -> [Val]
+    go GADT.TypeRzero                                         ()      = []
+    go (GADT.TypeRscalar t)                                   x       = [ upcastScalar t x ]
+    go (GADT.TypeRsnoc GADT.TypeRzero b@GADT.TypeRsnoc{})     ((), x) = [ tup (go b x) ]
+    go (GADT.TypeRsnoc a@GADT.TypeRsnoc{} b@GADT.TypeRsnoc{}) (x, y)  = [ tup (go a x), tup (go b y) ]
+    go (GADT.TypeRsnoc a b)                                   (x, y)  = go a x ++ go b y
+
+
+-- Expressions
+
+upcast :: forall env t. GADT.OpenExp env t -> Exp
+upcast exp =
+  case exp of
+    GADT.Var ix         -> Var (upcastTypeR (GADT.eltType (undefined::t))) (GADT.idxToInt ix)
+    GADT.Let a b        -> Let (upcast a) (upcast b)
+    GADT.Const c        -> let t = GADT.eltType (undefined::t)
+                           in  Const (upcastTypeR t) (upcastConstR t c)
+    GADT.Prod p         -> prod p
+    GADT.Prj ix p       -> Prj (GADT.prodIdxToInt ix) (upcast p)
+    GADT.If p t e       -> If (upcast p) (upcast t) (upcast e)
+    GADT.PrimApp f x    -> PrimApp (prim f) (upcast x)
+
+  where
+    prod :: GADT.Prod (GADT.OpenExp env) p -> Exp
+    prod =
+      let go :: GADT.Prod (GADT.OpenExp env) p -> [Exp]
+          go GADT.EmptyProd      = []
+          go (GADT.PushProd p e) = upcast e : go p
+      in
+      Prod . reverse . go
+
+    prim :: GADT.PrimFun f -> PrimFun
+    prim (GADT.PrimAdd t) = PrimAdd (upcastNumType t)
+    prim (GADT.PrimMul t) = PrimMul (upcastNumType t)
+    prim GADT.PrimToFloat = PrimToFloat
 
