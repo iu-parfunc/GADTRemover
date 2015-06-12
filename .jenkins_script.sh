acb6a922d4ee3714e81b6bb3a427f999c42b8157
Ryan Newton <rrnewton@gmail.com>
2015-06-11 20:55:52 -0400

    Add cabal test to the jenkins script

--- .jenkins_script.sh
+++ .jenkins_script.sh
@@ -5,9 +5,16 @@ ENVSCRIPT=$HOME/rn_jenkins_scripts/acquire_ghc.sh
 if [ -f "$ENVSCRIPT" ]; then source "$ENVSCRIPT"; fi
 if [ "$CABAL" == ""  ]; then CABAL=cabal; fi
 
+which -a $CABAL
+$CABAL --version
+
 $CABAL sandbox init
 
 OPTS=" -j --ghc-options=-j3 --run-tests "
 
 # For now just make sure everything installs:
 $CABAL install ./GADTRemover ./GADT_CastChallenge $OPTS
+
+cd ./GADT_CastChallenge/
+$CABAL sandbox init --sandbox=../.cabal-sandbox/
+$CABAL test
