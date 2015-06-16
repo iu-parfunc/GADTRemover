5292ee7ffc0deb70db9aea7007cff847ba421bc3
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2015-06-16 18:05:28 +1000

    add some basic benchmarks

---
+++ GADT_CastChallenge/Feldspar/Benchmark.hs
@@ -0,0 +1,56 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Feldspar.Benchmark
+  where
+
+import Feldspar.Example
+import qualified Feldspar.InterpGADT                    as GADT
+import qualified Feldspar.Hint                          as Hint
+import qualified Feldspar.ADT2                          as ADT2
+
+import Data.Typeable
+import Criterion.Main
+
+
+main :: IO ()
+main =
+  let fib_10            = fib 10
+      fib_20            = fib 20
+      pascal_10_5       = pascal 10 5
+
+      testADT :: ADT2.Exp -> Int
+      testADT adt =
+        case ADT2.downcastExp adt of
+          Just (ADT2.SealedExp e)
+            | Just gadt <- gcast e -> GADT.run gadt ()
+          _                        -> error "downcast failed"
+
+      testHint :: ADT2.Exp -> IO Int
+      testHint adt = do
+        gadt   <- Hint.downcastExp adt
+        return $! GADT.run gadt ()
+
+  in
+  defaultMain
+    [ env (return (ADT2.upcastExp fib_10)) $ \adt ->
+        bgroup "fib.10"
+          [ bench "run"   $ whnf (GADT.run fib_10) ()
+          , bench "adt2"  $ whnf testADT adt
+          , bench "hint"  $ whnfIO (testHint adt)
+          ]
+
+    , env (return (ADT2.upcastExp fib_20)) $ \adt ->
+        bgroup "fib.20"
+          [ bench "run"   $ whnf (GADT.run fib_20) ()
+          , bench "adt2"  $ whnf testADT adt
+          , bench "hint"  $ whnfIO (testHint adt)
+          ]
+
+    , env (return (ADT2.upcastExp pascal_10_5)) $ \adt ->
+        bgroup "pascal.10.5"
+          [ bench "run"   $ whnf (GADT.run pascal_10_5) ()
+          , bench "adt2"  $ whnf testADT adt
+          , bench "hint"  $ whnfIO (testHint adt)
+          ]
+    ]
+
