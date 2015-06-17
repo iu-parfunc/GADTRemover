8d403e63988ae079646fef1c19392fedf46ef4d7
Ryan Newton <rrnewton@gmail.com>
2015-06-16 22:51:02 -0400

    Develope another example

--- GADT_CastChallenge/ProblemCases.hs
+++ GADT_CastChallenge/ProblemCases.hs
@@ -9,6 +9,8 @@ import Data.Typeable
 import Text.Printf
 
 --------------------------------------------------------------------------------
+-- Ambiguity examples.
+--------------------------------------------------------------------------------
 
 -- Fails the ambiguity check:
 data Foo a where
@@ -22,10 +24,32 @@ t0 = Bar (Foo 'a')
 
 t1 = Bar' (Foo' (toDyn 'a'))
 
-
+-- Nothing can recover a type for Bar's argument here:
 data Foo' where
   Foo' :: Dynamic -> Foo'
   Bar' :: Foo' -> Foo'
  deriving Show
 
 --------------------------------------------------------------------------------
+-- How about erased variables flowing to kept phantom positions?
+--------------------------------------------------------------------------------
+
+data A x = A (B x) deriving Show
+
+data B a = B Int deriving Show
+
+-- Ghostbust A.x, checked:
+data A' = A' (B Empty)
+-- "Empty" here could also be Dynamic for consistency.
+-- data A' = A' (B Dynamic)
+
+data Empty
+
+-- We could disallow this scenario, but in some sense it is even
+-- *easier* to cast types with phantom vars:
+up_A :: Typeable x => A' -> Maybe (A x)
+up_A (A' (B i))  =
+  Just $ A (B i)
+
+t2 :: Maybe (A Char)
+t2 = up_A (A' (B 3))
