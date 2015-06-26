0dd264b4c5b94f0545e115b328c3ce1fc856779e
Tim Zakian <tzakian@indiana.edu>
2015-06-26 12:49:00 -0400

    Update, fix a few things, make it work with updated feldspar

--- Remover2/src/Ghostbuster/KindCheck.hs
+++ Remover2/src/Ghostbuster/KindCheck.hs
@@ -1,7 +1,5 @@
 {-# LANGUAGE RecordWildCards #-}
 -- | TODO: Need to make this handle mutually recursive data definitions
--- | TODO: How to deal with type variables that aren't bound by a quantifier? (ie., implicit foralls?)
---         OR can we always assume that any type variables will either be bound somewhere?
 
 module Ghostbuster.KindCheck where
 
@@ -19,37 +17,39 @@ type TKEnv = Map.Map Var Kind
 toDEnv :: [DDef] -> DEnv
 toDEnv ls = Map.fromList $ zip (map tyName ls) ls
 
+-- | This is for checking multiple DDefs at once that we see. We assume for
+--   now that something will not be refered to until it has been defined.
+--   kindCheckDDefs (toDEnv primitiveTypes) (Map.fromList [(mkVar "b", Star)]) feldspar_gadt
+kindCheckDDefs :: DEnv -> TKEnv -> [DDef] -> Either TypeError DEnv
+kindCheckDDefs denv tkenv (ddef : defs) = do
+  newEnv <- kindCheck denv tkenv ddef
+  kindCheckDDefs newEnv tkenv defs
+kindCheckDDefs denv tkenv [] = return $ denv
+
 -- Check it out!
--- mapM (kindCheck (toDEnv primitiveTypes) (Map.fromList [(mkVar "b", Star)])) feldspar_gadt
-kindCheck ::DEnv -> TKEnv -> DDef -> Either TypeError ()
+-- mapM (kindCheck (toDEnv (dd2 : dd3 : primitiveTypes)) (Map.fromList [(mkVar "b", Star)])) feldspar_gadt
+kindCheck :: DEnv -> TKEnv -> DDef -> Either TypeError DEnv
 kindCheck env tenv d@DDef{..} =
-  -- All kept and synthesized vars must be of kind *
+  -- All checked and synthesized vars must be of kind *
   if (allStars cVars) && (allStars sVars)
   -- Now check all the constructors for this datatype
-  then case mapM (kindConstr d env tenv) cases of
+  then case mapM (kindConstr d newEnv tenv) cases of
          Left a  -> Left $ "Failed to kindCheck data constructor: " ++ show a
-         Right k -> Right ()
+         Right k -> Right newEnv
   else Left "Failed to infer proper kinds for C and S args for data constructor"
  where
    allStars :: [(TyVar, Kind)] -> Bool
    allStars x = foldl (&&) True $ map ((== Star) . snd) x
+   newEnv :: DEnv
+   newEnv = (Map.union (toDEnv [d]) env)
 
 kindConstr :: DDef -> DEnv -> TKEnv -> KCons -> Either TypeError ()
-kindConstr parent@DDef{..} env tenv constr@KCons{..} =
-  -- Get the kind for each of the taus that lead up to this guy
-  let flds   = mapM (kindType env (tenv `Map.union` (Map.fromList (kVars ++ cVars ++ sVars)))) (map MonTy fields)
-      -- Get the kinds for all the output types
-      outpts = mapM (kindType env (tenv `Map.union` (Map.fromList (kVars ++ cVars ++ sVars)))) (map MonTy outputs)
-      -- Now get the kinds that we expect for all the outputs
-      tConstrKinds = map snd $ kVars ++ cVars ++ sVars
-   in case outpts of
-        Right ks ->
-          -- Now make sure that all the kinds that we expect the type
-          -- constructor to be applied to are actually applied to it
-          if tConstrKinds == ks then Right ()
-          else Left $ "Invalid Kinds for type constructor " ++
-                       show tyName ++ " Expected " ++ show tConstrKinds ++ " but received " ++ show ks
-        Left err -> Left $ "Unable to infer kind for " ++ err ++ " in constructor " ++ show conName
+kindConstr parent@DDef{..} env tenv constr@KCons{..} = do
+  -- Get the kind for each of the taus that lead up to this guy and make sure they all check out
+  flds <- mapM (kindType env (tenv `Map.union` (Map.fromList (kVars ++ cVars ++ sVars)))) (map MonTy fields)
+      -- Get the kinds for all the output types and make sure that the kinds check out there too
+  outpts <- mapM (kindType env (tenv `Map.union` (Map.fromList (kVars ++ cVars ++ sVars)))) (map MonTy outputs)
+  return ()
 
 -- kindType Map.empty Map.empty kindTyScheme1
 -- kindType Map.empty Map.empty kindTyScheme2
@@ -71,11 +71,11 @@ kindType env tenv tscheme =
         Nothing -> Left $ "Unbound type variable found! " ++ show tyVar
         Just k  -> Right k
     MonTy (ArrowTy monoTy1 monoTy2) -> do
-      mtk1 <- kindType env tenv (MonTy monoTy1)
-      mtk2 <- kindType env tenv (MonTy monoTy2)
-      return $ ArrowKind mtk1 mtk2
+      Star <- kindType env tenv (MonTy monoTy1)
+      Star <- kindType env tenv (MonTy monoTy2)
+      return $ Star -- ArrowKind mtk1 mtk2
     MonTy (TypeDictTy tname)        -> do
-      -- According to our typing rules, this guy had beeter have kind * so
+      -- According to our typing rules, this guy had better have kind * so
       -- enforce that here
       kindt <- kindType env tenv (MonTy (VarTy tname))
       if kindt /= Star
@@ -89,10 +89,9 @@ kindType env tenv tscheme =
                      in if kinds == tnameKinds
                         then return Star -- Only allow fully applied type constructors for now
                         else Left $ "Invalide type constructor application: " ++ show kinds
+    t -> error $ "FOund " ++ show t
 
 kindProg :: Prog -> DEnv -> Either TypeError ()
 kindProg = undefined
  -- Typecheck/infer
  -- Once we get the types go and run kindType on it
-
-
