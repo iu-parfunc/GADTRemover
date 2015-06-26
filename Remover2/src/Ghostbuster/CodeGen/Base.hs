f2df902aa2afc60d17de0fca11e5e79b7e3ab562
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2015-06-27 00:19:43 +1000

    this tentative commit snuck in...

--- Remover2/src/Ghostbuster/CodeGen/Base.hs
+++ Remover2/src/Ghostbuster/CodeGen/Base.hs
@@ -29,5 +29,5 @@ mkType (VarTy v)        = TyVar (varName v)
 mkType (ArrowTy a b)    = TyFun (mkType a) (mkType b)
 mkType (TupleTy tup)    = TyTuple Boxed (map mkType tup)
 mkType (ConTy c tys)    = foldl TyApp (TyCon (UnQual (varName c))) (map mkType tys)
-mkType (TypeDictTy v)   = mkType (ConTy "TypeDict" [VarTy v])
+mkType (TypeDictTy v)   = mkType (ConTy "TypeDict" [VarTy v])   -- TLM: ???
 
