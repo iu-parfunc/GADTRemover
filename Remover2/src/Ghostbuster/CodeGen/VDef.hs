b669eb90cc965f90e5da2ff05d3a6d0b0a16fec8
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2015-06-29 01:09:34 +1000

    start codegen for expressions

--- Remover2/src/Ghostbuster/CodeGen/VDef.hs
+++ Remover2/src/Ghostbuster/CodeGen/VDef.hs
@@ -3,10 +3,10 @@
 module Ghostbuster.CodeGen.VDef
   where
 
-import Ghostbuster.Types
-import Ghostbuster.CodeGen.Base
+import Ghostbuster.Types                                as G
+import Ghostbuster.CodeGen.Base                         as G
 
-import Language.Haskell.Exts
+import Language.Haskell.Exts                            as H
 import Language.Haskell.Exts.SrcLoc                     ( noLoc )
 
 
@@ -15,8 +15,14 @@ import Language.Haskell.Exts.SrcLoc                     ( noLoc )
 declOfVDef :: VDef -> [Decl]
 declOfVDef VDef{..} =
   [ mkTypeSig valName valTy
-  , FunBind [ ]
+  , FunBind body
   ]
+  where
+    body = case valExp of
+             ELam (x,_) (ECase x' es)
+              | G.EVar x == x' -> map (uncurry (topLevelFun valName)) es
+
+             _ -> error "TODO"
 
 
 -- Convert a type scheme into a type signature
@@ -27,4 +33,33 @@ mkTypeSig ident scheme
   $ case scheme of
       MonTy t    -> mkType t
       ForAll a t -> TyForall (Just (map (uncurry mkTyVarBind) a)) [] (mkType t)
+
+
+topLevelFun :: Var -> G.Pat -> G.Exp -> Match
+topLevelFun fn p e =
+  Match
+    noLoc                       -- source location
+    (varName fn)                -- name of the function
+    [mkPat p]                   -- patterns, to be matched against a value
+    Nothing                     -- type signature
+    (mkRhs e)                   -- the right hand side of the function, pattern, or case alternative
+    (BDecls [])                 -- binding group for let or where clause
+
+mkExp :: G.Exp -> H.Exp
+mkExp (G.EK n)          = var (varName n)        -- TLM ???
+mkExp (G.EVar n)        = var (varName n)
+-- mkExp (G.ELam (x,_) b)  = lamE noLoc undefined undefined
+mkExp (G.EApp a b)      = app (mkExp a) (mkExp b)
+mkExp (G.ECase e ps)    = caseE (mkExp e) (map (uncurry mkAlt) ps)
+mkExp _                 = error "TODO"
+
+mkAlt :: G.Pat -> G.Exp -> H.Alt
+mkAlt p e =
+  Alt noLoc (mkPat p) (mkRhs e) (BDecls [])
+
+mkPat :: G.Pat -> H.Pat
+mkPat (Pat pn pv) = pParen $ pApp (varName pn) (map (pvar . varName) pv)
+
+mkRhs :: G.Exp -> H.Rhs
+mkRhs = UnGuardedRhs . mkExp
 
