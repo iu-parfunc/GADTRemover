95070f48fce932ac20c05c4e90a0ded04cde339e
Ryan Newton <rrnewton@gmail.com>
2015-06-28 08:12:04 -0400

    Fix cast challenge build.  Problem in Hint.  Some warning fixes.

--- CastChallenge/Feldspar/GADT.hs
+++ CastChallenge/Feldspar/GADT.hs
@@ -2,12 +2,14 @@
 {-# LANGUAGE GADTs             #-}
 {-# LANGUAGE KindSignatures    #-}
 {-# LANGUAGE RoleAnnotations   #-}
+{-# LANGUAGE DeriveDataTypeable #-}
 
 -- | Mini feldspar GADT, copied from:
 --     https://github.com/shayan-najd/MiniFeldspar/
 
 module Feldspar.GADT where
 
+import Data.Typeable (Typeable)
 import Text.PrettyPrint.Leijen
 
 -- GADT representation.
@@ -24,6 +26,7 @@ type role Var nominal nominal
 data Var e a where
   Zro :: Var (e,a) a  -- This requires role nominal for the environment param.
   Suc :: Var e a -> Var (e,b) a -- So does this
+ deriving Typeable
 
 type role Exp nominal nominal
 data Exp (e :: *) (a :: *) where
@@ -33,16 +36,19 @@ data Exp (e :: *) (a :: *) where
   Var :: Var e a -> Exp e a
   Abs :: Typ a -> Exp (e,a) b -> Exp e (a -> b)
   App :: Exp e (a -> b) -> Exp e a -> Exp e b
+ deriving Typeable
 
 -- Types (Singleton)
 data Typ a where
   Int :: Typ Int
   Arr :: Typ a -> Typ b -> Typ (a -> b)
+ deriving Typeable
 
 -- Environment (Singleton)
 data Env e where
   Emp :: Env ()
   Ext :: Env e -> Typ a -> Env (e,a)
+ deriving Typeable
 
 class Elt a where
   eltType :: Typ a
@@ -117,4 +123,3 @@ instance Show (Exp env a) where
 
 instance Show (Typ a) where
   show = show . prettyType
-
