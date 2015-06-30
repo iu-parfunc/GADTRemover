8f3fc3d0dec631e658a2aed95d0960bfc28b50dc
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2015-06-30 10:15:50 +1000

    unused imports

--- Remover2/src/Ghostbuster/CodeGen/VDef.hs
+++ Remover2/src/Ghostbuster/CodeGen/VDef.hs
@@ -4,11 +4,9 @@ module Ghostbuster.CodeGen.VDef
   where
 
 import Ghostbuster.Types                                as G
-import Ghostbuster.CodeGen.Base                         as G
 import Ghostbuster.CodeGen.Exp                          as G
 
 import Language.Haskell.Exts                            as H
-import Language.Haskell.Exts.SrcLoc                     ( noLoc )
 
 
 -- Convert a value definition into a set of top-level declarations
