2b1b6e7639db4771c8a8edad40c24f52b7428637
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2015-07-01 23:33:37 +1000

    list example: type var 'a' should be stripped

--- Remover2/src/Ghostbuster/Examples/Tiny.hs
+++ Remover2/src/Ghostbuster/Examples/Tiny.hs
@@ -224,7 +224,7 @@ p10_mut_add_even = Prog
 
 p11_bustedList :: Prog
 p11_bustedList = Prog
-  [DDef "List" [] [("a", Star)] []
+  [DDef "List" [] [] [("a", Star)]
       [ KCons "Nil" [] ["a"]
       , KCons "Cons" ["a", ConTy "List" ["a"]] ["a"]
       ]
