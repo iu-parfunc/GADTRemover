fe38897ec8c0213d0727fc045b221811a23e83f2
Ryan Newton <rrnewton@gmail.com>
2015-10-12 08:35:16 -0400

    switch to stack, but having some test failures on lts-3

--- .jenkins_script.sh
+++ .jenkins_script.sh
@@ -3,28 +3,10 @@
 set -xe
 
 # This is specific to our testing setup at IU:
-if [ "$JENKINS_GHC" == ""  ]; then JENKINS_GHC=7.8.4; fi
-ENVSCRIPT=$HOME/rn_jenkins_scripts/acquire_ghc.sh
 if [ -f "$ENVSCRIPT" ]; then source "$ENVSCRIPT"; fi
-if [ "$CABAL" == ""  ]; then CABAL=cabal; fi
+if [ "$STACK" == ""  ]; then STACK=stack; fi
+which -a $STACK
+$STACK --version
 
-which -a $CABAL
-$CABAL --version
-
-$CABAL sandbox init
-
-OPTS=" -j --ghc-options=-j3 --enable-tests --only-dependencies "
-
-PKGS="./GADTRemover ./CastChallenge ./Remover2"
-
-# Install and run-tests in one step:
-$CABAL install $PKGS $OPTS $@
-
-top=`pwd`
-for pkg in $PKGS; do
-    cd "$pkg"
-    $CABAL sandbox init --sandbox=../.cabal-sandbox/
-    $CABAL configure --enable-tests
-    $CABAL test --show-details=always
-    cd "$top";
-done
+$STACK setup
+$STACK test
