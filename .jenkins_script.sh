e814411c8142450d0ee6cfcd2fb28d8b9b40d2a1
Ryan Newton <rrnewton@gmail.com>
2015-11-06 15:44:45 -0500

    Add fuzz test to the validate script

--- .jenkins_script.sh
+++ .jenkins_script.sh
@@ -17,6 +17,11 @@ RUN="stack exec ghostbust --"
 # Hack, for now assume Stack has installed GHC:
 GHC=~/.stack/programs/x86_64-linux/ghc-7.10.2/bin/ghc
 
-$RUN Remover2/examples/MiniFeldspar.hs Remover2/examples/MiniFeldspar_busted.hs
+# First, run one example by hand.
 
+$RUN Remover2/examples/MiniFeldspar.hs Remover2/examples/MiniFeldspar_busted.hs
 $GHC Remover2/examples/MiniFeldspar_busted.hs
+
+# Second, run in fuzz test mode:
+
+$RUN --fuzz Remover2/examples/MiniFeldspar.hs
