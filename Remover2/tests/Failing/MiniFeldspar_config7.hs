79f1d48af507a2bd35e32db12bcff4005b6fe9e3
Ryan Newton <rrnewton@gmail.com>
2015-11-11 06:37:55 -0500

    Add a failing test file CC #13

---
+++ Remover2/tests/Failing/MiniFeldspar_config7.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# OPTIONS_GHC -fdefer-type-errors #-}
+module Ghostbuster where
+import Prelude hiding (Int, Maybe(..), Bool(..))
+-- for the Ghostbust data type that we use in the annotations
+import Ghostbuster.Parser.Prog
+
+-- | These particular erasure settings revealed issue #13:
+
+{-# ANN Var (Ghostbust [] [env] [arg]) #-}
+data Var env arg where
+  Zro :: Var (e,a) a  -- This requires role nominal for the environment param.
+  Suc :: Var e a -> Var (e,b) a -- So does this
+
+{-# ANN Exp (Ghostbust [env] [] [arg]) #-}
+data Exp env arg where
+  Con :: Int -> Exp e Int
+  Add :: Exp e Int -> Exp e Int -> Exp e Int
+  Mul :: Exp e Int -> Exp e Int -> Exp e Int
+  Var :: Var e a -> Exp e a
+  Abs :: Typ a -> Exp (e,a) b -> Exp e (a -> b)
+  App :: Exp e (a -> b) -> Exp e a -> Exp e b
+
+{-# ANN Typ (Ghostbust [] [arg] []) #-}
+data Typ arg where
+  Int :: Typ Int
+  Arr :: Typ a -> Typ b -> Typ (a -> b)
+
+
+-- Random extra code to test what the framework does with it.
+foo :: a -> a
+foo x = x
