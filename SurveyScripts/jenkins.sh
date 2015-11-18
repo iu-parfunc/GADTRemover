50be22b9f9a8618a936292d268f0b44193d7e29b
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2015-11-18 01:23:29 -0500

    jenkins: csvstat the output csv files so they are in the log

--- SurveyScripts/jenkins.sh
+++ SurveyScripts/jenkins.sh
@@ -103,6 +103,7 @@ n=0
 for csvfile in `cat all_csvs.txt`; do
     echo "Filing away: $csvfile"
     cp "$csvfile" "$outdir/$((n++))_"`basename $csvfile`
+    csvstat $csvfile
 done
 
 find "./data/4_compiled_ddefs/" -name "*.log" >> logfiles.txt
